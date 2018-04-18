b6cf06809fe658147087e95e1f76652dbee3c40e
pawelpodgorski <34062952+pawelpodgorski@users.noreply.github.com>
2018-04-18 11:22:18 -0400

    Add files via upload

---
+++ src/MainClass2 - TEST.java
@@ -0,0 +1,24 @@
+// Test comment to test scheduler
+public class MainClass {
+
+	public static void aMethod(String s1,String s2,String s3,String s4,String s5,String s6,String s7,String s8){
+		
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello WOrld");
+	
+		try{
+	
+			int x = 10/4;
+			System.out.println("the value of x: " + x);
+	
+		}catch(Exception e){
+		}
+	}
+}
