7478b4ae953301697c3f28f84ee3fb957f2731dc
pawelpodgorski <34062952+pawelpodgorski@users.noreply.github.com>
2018-05-01 21:14:47 -0400

    ENG Testing

---
+++ src/Exc 1 Test_ENG.py
@@ -0,0 +1,14 @@
+pi = 3.14159
+
+my_fname = 'TestF'
+my_lname = 'TestL'
+
+print(my_fname, my_lname, sep='\t')
+
+
+print('{:.4f} {:.4f} {:.8f}'.format(pi, pi, pi))
+
+print('%s ', format(my_name) )
+
+
+
---
+++ src/dogClass_ENG.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 08:45:41 2018
+
+@author: 
+"""
+
+class dogClass():
+    def _init_(self, name, weight, breed):
+        self.name = name
+        self.weight = weight
+        self.breed = breed
+        
+    def bark(self):
+        print('woof, woof, woof')
+        
+    def getDogInfo(self):
+        return self.name + ' : '
+        
